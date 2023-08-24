993bcfb1a33588ac087d3bd3d56a2ac459093b53
Julien Dutschke <jdutschk@k2r3p1.42mulhouse.fr>
2023-08-24 18:14:44 +0200

    tentative de rajout du nickname pas au point

--- Serveur.hpp
+++ Serveur.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   serveur.hpp                                        :+:      :+:    :+:   */
+/*   Serveur.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdutschk <jdutschk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:19:56 by jdutschk          #+#    #+#             */
-/*   Updated: 2023/08/23 18:18:16 by jdutschk         ###   ########.fr       */
+/*   Updated: 2023/08/24 18:01:30 by jdutschk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,13 @@
 # include <netinet/in.h>
 
 # define MAX_CLIENTS 10
-# define PORT 4242
+
+#define NO_PWD -2
+#define NO_NKN -1
 
 struct ClientInfo
 {
+	std::string name;
     sockaddr_in clientAddr;
 	int status;
 };
@@ -37,16 +40,21 @@ class Serveur
 	int	serverSocket_fd;
 	std::map <int, ClientInfo> mapClients;
 	std::vector<int> list_Clients_fd;
-	
+	std::string password;
+	int port;
+
 	public:
-	Serveur();
+	Serveur(int port);
 	~Serveur();
 
-
+	void set_port(int pt);
+	int get_port();
+	void set_password(std::string pass);
+	std::string get_password();
 	void Make_Sets_Sockets(int serverSocket_fd, const std::vector<int>& list_Clients_fd, fd_set& Sets_Sockets);
 	void launch_serveur();
 	void read_client_message(std::vector<int>& list_Clients_fd, fd_set Sets_Sockets);
-	void print_message_client(const std::vector<int>& list_Clients_fd, int i, char *message);
+	void execute_message(int fd_key, char *message);
 	void deconnect_client(std::vector<int>& list_Clients_fd, int i);
 	void add_new_connection(int serverSocket_fd, fd_set Sets_Sockets, std::vector<int>& list_Clients_fd);
 };
--- serveur.cpp
+++ serveur.cpp
@@ -6,11 +6,31 @@
 /*   By: jdutschk <jdutschk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:09:02 by jdutschk          #+#    #+#             */
-/*   Updated: 2023/08/23 18:31:40 by jdutschk         ###   ########.fr       */
+/*   Updated: 2023/08/24 18:13:30 by jdutschk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serveur.hpp"
+
+void Serveur::set_port(int pt)
+{
+	port = pt;
+}
+
+int Serveur::get_port()
+{
+	return (port);
+}
+
+void Serveur::set_password(std::string pass)
+{
+	password = pass;
+}
+
+std::string Serveur::get_password()
+{
+	return(password);
+}
 
 Serveur::~Serveur()
 {
@@ -18,7 +38,7 @@ Serveur::~Serveur()
 	close(serverSocket_fd);
 }
 
-Serveur::Serveur()
+Serveur::Serveur(int port)
 {
     sockaddr_in serverAddr;
     int         serverSocket_fd;
@@ -34,7 +54,7 @@ Serveur::Serveur()
 	}
 
     serverAddr.sin_family = AF_INET; //set la famille d'addresse en ipv4
-    serverAddr.sin_port = htons(PORT); //convertis le port 4242
+    serverAddr.sin_port = htons(port); //convertis le port 4242
     serverAddr.sin_addr.s_addr = INADDR_ANY; //adresse autoriser a se connecter a se socket 
 	
 	
@@ -112,7 +132,7 @@ void Serveur::add_new_connection(int serverSocket_fd, fd_set Sets_Sockets, std::
 				{
                     list_Clients_fd[i] = clientSocket_fd;
 					mapClients[clientSocket_fd].clientAddr = clientAddr;
-					mapClients[clientSocket_fd].status = 0;
+					mapClients[clientSocket_fd].status = NO_PWD;
                     break;
                 }
             }
@@ -135,19 +155,38 @@ void Serveur::deconnect_client(std::vector<int>& list_Clients_fd, int i)
 	list_Clients_fd[i] = 0;
 }
 
-void Serveur::print_message_client(const std::vector<int>& list_Clients_fd, int i, char *message)
+void Serveur::execute_message(int fd_key, char *message)
 {
+	std::string input = message;
 	
-	std::cout << "Message reçu du client " << list_Clients_fd[i] << " : " << message << std::endl;
-	const char *response = "Message reçu par le serveur !";
-	
-	send(list_Clients_fd[i], response, strlen(response), 0);
+	if (mapClients[fd_key].status == NO_PWD)
+	{
+		if (input != password)
+		{
+			std::cout << "le client donne [" + input + "]" << "et le mdp original [" + password + "]";
+			send(fd_key, "Wrong password\nTry again\n", 25, 0);
+		}
+		else
+		{
+			mapClients[fd_key].status = NO_NKN;
+			send(fd_key, "password ok\nchoose a nickname\n", 30, 0);
+		}
+	}
+	else if (mapClients[fd_key].status == NO_NKN)
+	{
+		if (input.find("/Nickname") == 0)
+		{
+			mapClients[fd_key].name = input.substr(10);
+			send(fd_key, "Nickname set\n", 13, 0);
+		}
+		else
+			send(fd_key, "Please choose a Nickname first with /Nickname\n", 46, 0);
+	}
 }
 
 void Serveur::read_client_message(std::vector<int>& list_Clients_fd, fd_set Sets_Sockets)
 {
 	char message[1024];
-
 	
 	for (int i = 0; i < MAX_CLIENTS; i++) 
 		{
@@ -155,21 +194,19 @@ void Serveur::read_client_message(std::vector<int>& list_Clients_fd, fd_set Sets
 			if (FD_ISSET(list_Clients_fd[i], &Sets_Sockets))
 			{
 				//stock les infos lues
-                int bytesRead = recv(list_Clients_fd[i], message, sizeof(message), 0);
-                
+            	int bytesRead = recv(list_Clients_fd[i], message, sizeof(message), 0);
+				
 				//libere la place en cas de deconnections 
 				if (bytesRead <= 0)
 					deconnect_client(list_Clients_fd, i);
 				else //affiche le message sur le serveur et envois une reponse au client vis a vis du serveur
 				{
-                    message[bytesRead] = '\0';
-					print_message_client(list_Clients_fd, i, message);
+            		message[bytesRead] = '\0';
+					execute_message(list_Clients_fd[i], message);
                 }
             }
         }
 }
-
-
 
 void Serveur::launch_serveur()
 {
@@ -192,10 +229,21 @@ void Serveur::launch_serveur()
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	Serveur Serveur;
-  
+	if (argc != 3)
+	{
+        std::cout << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+        return 1;
+    }
+
+	if (atoi(argv[1]) == 0)
+	{
+		std::cout <<"Not valid port\n";
+		return 1;
+	}
+	Serveur Serveur(atoi(argv[1]));
+	Serveur.set_password(argv[2]);
     Serveur.launch_serveur();
 
     return 0;
