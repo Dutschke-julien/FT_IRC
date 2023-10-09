ba001a15d8c21e8baa398634c16d75573c43d142
Dutschke julien <jdutschk@student.42mulhouse.fr>
2023-10-09 17:04:31 +0200

    Update User_cmd.cpp

--- User_cmd.cpp
+++ User_cmd.cpp
@@ -14,8 +14,13 @@
 
 void Serveur::cmd_User(std::string cmd, int fd_key)
 {
-	if(_mapClients[fd_key].get_status() == 0)
+
+	if (_mapClients[fd_key].get_status() == NO_PASS || _mapClients[fd_key].get_status() == NO_NICK)
 	{
+		__mapClients[fd_key].set_status(KICK);
+		return;
+	}
+	
 		cmd.erase(cmd.find_last_not_of("\r\n") + 1);
 		std::istringstream iss(cmd);
 
@@ -30,49 +35,22 @@ void Serveur::cmd_User(std::string cmd, int fd_key)
 		
 		if (isNickTaken(_register, username))
 		{
-			std::string erreur = ":42Mulhouse 462 :Vous etes deja connecter depuis une autre interface\r\n";
-			send(fd_key, erreur.c_str(), erreur.length(), 0);
-
+			username += "_";
+			_mapClients[fd_key].set_realname(real_Name);
+			_register.push_back(username);
+			std::string bienvenue = ":42Mulhouse 001 " + username + " :Bienvenue sur votre serveur IRC\r\n";
+			send(fd_key, bienvenue.c_str(), bienvenue.length(), 0);
+			_mapClients[fd_key].set_status(ALL_OK);
 		}
 		else
 		{
 			_mapClients[fd_key].set_realname(real_Name);
 			_register.push_back(username);
-			std::string bienvenue = ":42Mulhouse 001 " + username + " :Vous pouvez entrer votre PASS\r\n";
+			std::string bienvenue = ":42Mulhouse 001 " + username + " :Bienvenue sur votre serveur IRC\r\n";
 			send(fd_key, bienvenue.c_str(), bienvenue.length(), 0);
-			_mapClients[fd_key].set_status(1);
-		}
-	}
-		
-	
-	
-	else if (_mapClients[fd_key].get_status() == 1)
-	{
-		cmd.erase(cmd.find_last_not_of("\r\n") + 1);
-		std::istringstream iss(cmd);
-		std::string mdp;
-		iss >> mdp;
-		if (mdp != this->get_password())
-		{
-			std::string erreur = ":42Mulhouse 464 :Mot de passe incorrect. Veuillez vérifier votre mot de passe.\r\n";
-			send(fd_key, erreur.c_str(), erreur.length(), 0);
-		}
-		else
-		{
-			_mapClients[fd_key].set_status(2);
-			std::string bienvenue = ":42Mulhouse 001 " + _mapClients[fd_key].get_nickname() + " :Bienvenue sur le serveur IRC. Vous êtes maintenant connecté.\r\n";
-			send(fd_key, bienvenue.c_str(), bienvenue.length(), 0);
-
+			_mapClients[fd_key].set_status(ALL_OK);
 		}
 	}
 
-
-	
-	else
-	{
-		std::cout << "la valeur du status " <<  _mapClients[fd_key].get_status() << std::endl;
-		std::string erreur = ":42Mulhouse 462 * :Vous etes deja entierement enregistré\r\n";
-		send(fd_key, erreur.c_str(), erreur.length(), 0);
-	}
 }
 
